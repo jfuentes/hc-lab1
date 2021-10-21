d368f5cc2d2d0a10046f2cc769e51ba0e4c7b3f5
Joel Fuentes <jfuentes@ubiobio.cl>
2021-10-21 15:24:13 -0300

    minor changes

--- lab1.cpp
+++ lab1.cpp
@@ -5,7 +5,7 @@
 
 #include <CL/sycl.hpp>
 
-constexpr int N = 50;
+constexpr int N = 20;
 using namespace sycl;
 
 int main(){
@@ -19,17 +19,28 @@ int main(){
     std::vector<int> A(N);
     std::vector<int> B(N);
     std::vector<int> C(N);	
-    buffer buff(A);
-    buffer buff(B);
-    buffer buff(C);
+    buffer buffA(A);
+    buffer buffB(B);
+    buffer buffC(C);;
 
     // kernel submission to hardware device
     q.submit([&](handler& h){
         // Add your code here
     };
 
+    std::cout << "A: ";
     for (int i=0; i<N; i++) 
-		    std::cout << v[i] << " ";
+		    std::cout << A[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "B: ";
+    for (int i=0; i<N; i++) 
+		    std::cout << B[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "C: ";
+    for (int i=0; i<N; i++) 
+		    std::cout << C[i] << " ";
     std::cout << std::endl;
 
 	return 0;
